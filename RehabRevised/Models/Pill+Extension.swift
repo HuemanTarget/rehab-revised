625c6adcd1f78f755efbb508414e5bd7cb768c60
HuemanTarget <joshbasche@live.com>
2021-05-25 10:28:36 -0500

    create Pill BaseModel Extension

---
+++ RehabRevised/Models/Pill+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Pill+Extension.swift
+//  RehabRevised
+//
+//  Created by Joshua Basche on 5/25/21.
+//
+
+import Foundation
+import CoreData
+
+extension Pill: BaseModel {
+  static func byID<T>(id: NSManagedObjectID) -> T? where T : NSManagedObject {
+    do {
+      return try viewContext.existingObject(with: id) as? T
+    } catch {
+      return nil
+    }
+  }
+}
