977854bcabfadd10d4faf05d48970b043b0c7b06
HuemanTarget <joshbasche@live.com>
2021-05-25 09:33:11 -0500

    first commit

---
+++ RehabRevised/RehabRevisedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RehabRevisedApp.swift
+//  RehabRevised
+//
+//  Created by Joshua Basche on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct RehabRevisedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
